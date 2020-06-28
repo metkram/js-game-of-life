dd70d7179795a764e5c81e95759d5cf48df30c5e
unknown <metkraming@gmail.com>
2020-06-28 14:25:59 +0300

    create fullPage grid and matrix

--- game.js
+++ game.js
@@ -3,20 +3,49 @@
 class Game {
   constructor() {
     this.torus = new Torus();
+    console.log(this.torus.matrix[5]);
   }
 }
 
 class Torus {
   constructor() {
-    this.shell = document.createElement("div");
-    this.shell.id = "torus";
-    for (let i = 0; i < 5776; i++) {
-      let cell = document.createElement("div");
-      cell.className = "empty-cell";
-      cell.innerText = i;
-      this.shell.append(cell);
+    this.gridOnPage();
+    this.blocks = document.body.querySelectorAll(".empty-cell");
+    this.matrix = this.newMatrix();
+  }
+  get x() {
+    return Math.floor(document.body.clientWidth / 12);
+  }
+  get y() {
+    return Math.floor(document.body.clientHeight / 12);
+  }
+  newMatrix() {
+    let matrix = [];
+    for (let i = 0; i < this.y; i++) {
+      let line = [];
+      for (let u = 0; u < this.x; u++) {
+        let cell = {};
+        cell.x = i;
+        cell.y = u;
+        cell.alive = false;
+        line.push(cell);
+      }
+      matrix.push(line);
     }
-    document.body.append(this.shell);
+    return matrix;
+  }
+  gridOnPage() {
+    for (let i = 0; i < this.y; i++) {
+      let line = document.createElement("div");
+      line.className = "cell-line";
+      for (let u = 0; u < this.x; u++) {
+        let cell = document.createElement("div");
+        cell.className = "empty-cell";
+        cell.innerText = i * this.y + u;
+        line.append(cell);
+      }
+      document.body.append(line);
+    }
   }
 }
 
