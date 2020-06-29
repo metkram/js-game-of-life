fae4c9932332e3d8906300a29958cc488d146387
unknown <metkraming@gmail.com>
2020-06-29 14:45:02 +0300

    add functionality to add first 10 alive cells and render it on the page

--- game.js
+++ game.js
@@ -3,7 +3,25 @@
 class Game {
   constructor() {
     this.torus = new Torus();
-    console.log(this.torus.matrix[5]);
+    console.log(this.torus.matrix[0]);
+    this.cells = [];
+    this.start = false;
+    setTimeout(() => this.step(), 1000);
+  }
+  theCreationOfAdam(e) {
+    //have to add check for the same cells
+    this.cells.push(e.target.innerText.split(","));
+    this.torus.addAliveCells(this.cells);
+    console.log(this.cells);
+  }
+  step() {
+    if (!this.start) return;
+    if (this.cells) {
+      this.torus.addAliveCells(this.cells);
+      this.cells = [];
+    }
+    this.torus.renderMatrix();
+    setTimeout(() => this.step(), 1000);
   }
 }
 
@@ -41,12 +59,33 @@ class Torus {
       for (let u = 0; u < this.x; u++) {
         let cell = document.createElement("div");
         cell.className = "empty-cell";
-        cell.innerText = i * this.y + u;
+        cell.innerText = i + "," + u;
         line.append(cell);
       }
       document.body.append(line);
     }
   }
+  renderMatrix() {
+    for (let i = 0; i < this.matrix.length; i++) {
+      for (let u = 0; u < this.matrix[i].length; u++) {
+        if (this.matrix[i][u].alive) this.blocks[i * this.x + u].classList.add("alive-cell");
+      }
+    }
+  }
+  addAliveCells(cells) {
+    for (let cell of cells) {
+      this.matrix[cell[0]][cell[1]].alive = true;
+    }
+    this.renderMatrix();
+  }
 }
 
 let newLife = new Game();
+let add = function(e) {
+  newLife.theCreationOfAdam(e);
+  if (newLife.cells.length > 9) {
+    newLife.start = true;
+    document.removeEventListener("click", add);
+  }
+}
+document.addEventListener("click", add);
